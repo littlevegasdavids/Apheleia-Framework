6c5f923809c1a10081563cae58e2b2002dc8299f
thesamegatsby <rcd999@protonmail.com>
2022-07-25 13:37:27 +0200

    Browser and tablet view

--- src/Components/Customer/ChangePassword.svelte
+++ src/Components/Customer/ChangePassword.svelte
@@ -74,30 +74,39 @@
     }
 </script>
 
-<h1 class="font-bold text-3xl text-center pb-3">Change Customer Password</h1>
-
-<div class="grid justify-items-center gap-4">
-    <div>
-        <p>Old Password</p>
-        <input class="input input-bordered w-full" bind:value={oldPassword} placeholder="Enter Old Password" type="password"/>
+<div class="grid grid-cols-1 justify-items-center max-w-screen-tablet mx-auto">
+    <div class="divide-y divide-soli w-full">
+        <h1 class="font-bold text-3xl text-center pb-3 tablet:text-4xl">Change Customer Password</h1>
+        <p></p>
     </div>
+
+    <div class="grid justify-items-center gap-4 mt-5">
+        <div>
+            <p class="tablet:text-xl">Old Password</p>
+            <input class="input input-bordered tablet:input-lg" bind:value={oldPassword} placeholder="Enter Old Password" type="password"/>
+        </div>
+        
+        <div>
+            <p class="tablet:text-xl">New Password</p>
+            <input class="input input-bordered tablet:input-lg" bind:value={newPassword} placeholder="Enter New Password" type="password"/>
+        </div>
     
-    <div>
-        <p>New Password</p>
-        <input class="input input-bordered w-full" bind:value={newPassword} placeholder="Enter New Password" type="password"/>
+        <div>
+            <p class="tablet:text-xl">Confirm Password</p>
+            <input class="input input-bordered tablet:input-lg" bind:value={confirmPassword} placeholder="Confirm Password" type="password"/>
+        </div>
+    
+        {#if showError}
+            <p class="font-bold text-red-600 text-center">{errorMessage}</p>
+        {:else if showSuccess}
+            <p class="text-center">Successfully changed your password. Redirecting you in 5 seconds</p>
+        {/if}
+        <div class="grid grid-cols-1 gap-4 tablet:grid-cols-2">
+            <button class="btn btn-success rounded-md" on:click={changePassword} disabled={disbaleBtn}>Save</button>
+            <button class="btn btn-primary rounded-md" on:click={()=>window.location.href = "/customer"}>Back</button>
+        </div>
+        
+    
     </div>
-
-    <div>
-        <p>Confirm Password</p>
-        <input class="input input-bordered w-full" bind:value={confirmPassword} placeholder="Confirm Password" type="password"/>
-    </div>
-
-    {#if showError}
-        <p class="font-bold text-red-600 text-center">{errorMessage}</p>
-    {:else if showSuccess}
-        <p class="text-center">Successfully changed your password. Redirecting you in 5 seconds</p>
-    {/if}
-    <button class="btn btn-success rounded-md" on:click={changePassword} disabled={disbaleBtn}>Save</button>
-    <button class="btn btn-primary rounded-md" on:click={()=>window.location.href = "/customer"}>Back</button>
-
 </div>
+
