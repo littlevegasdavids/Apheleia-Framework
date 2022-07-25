954e6ee2529369a551a34b7521eaddb3f6249329
thesamegatsby <rcd999@protonmail.com>
2022-07-25 13:43:28 +0200

    Browser and tablet view

--- src/Components/Customer/ChangeName.svelte
+++ src/Components/Customer/ChangeName.svelte
@@ -45,21 +45,28 @@
     }
 </script>
 
-<h1 class="font-bold text-center text-3xl pb-3">Change Name</h1>
-
-<div class="grid justify-items-center gap-4">
-    <div>
-        <p class="text-center">Enter the new name you would like to change to :</p>
-        <input class="input input-bordered w-full" type="text" bind:value={newName} placeholder="New Name"/>
+<div class = "grid grid-cols-1 justify-items-center max-w-screen-tablet mx-auto">
+    <div class="divide-y divide-solid w-full">
+        <h1 class="font-bold text-center text-3xl pb-3">Change Name</h1>
+        <p></p>
     </div>
-    
-    {#if showError}
-        <p class="font-bold text-red-600 text-center">{errorMessage}</p>
-    {:else}
-        <p class="text-center">(Note please your real name so that we can associated any orders with you)</p>
-    {/if}
-    
-    <button class="btn btn-success rounded-md w-4/6" on:click={changeName}>Save</button>
-    <button class="btn btn-primary rounded-md w-4/6" on:click={()=>window.location.href = "/customer"}>Back</button>
 
+    <div class="grid justify-items-center gap-4 mt-5">
+        <div>
+            <p class="text-center tablet:text-xl pb-1">Enter the new name you would like to change to :</p>
+            <input class="input input-bordered w-full" type="text" bind:value={newName} placeholder="New Name"/>
+        </div>
+        
+        {#if showError}
+            <p class="font-bold text-red-600 text-center">{errorMessage}</p>
+        {:else}
+            <p class="text-center tablet:text-lg">(Note please your real name so that we can associated any orders with you)</p>
+        {/if}
+        <div class="grid grid-cols-1 gap-4 tablet:grid-cols-2">
+            <button class="btn btn-success rounded-md" on:click={changeName}>Save</button>
+            <button class="btn btn-primary rounded-md" on:click={()=>window.location.href = "/customer"}>Back</button>
+        </div>
+        
+
+    </div>
 </div>
