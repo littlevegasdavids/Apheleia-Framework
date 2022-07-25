1183c674983da5878936db132472cfdc188f7d90
thesamegatsby <rcd999@protonmail.com>
2022-07-25 16:18:48 +0200

    Made home page more narrow

--- src/Components/HomePage.svelte
+++ src/Components/HomePage.svelte
@@ -3,7 +3,7 @@
     import FAQ from '../Components/Home/Faq.svelte'
 </script>
 
-<div class="grid divide-y divide-solid">
+<div class="grid divide-y divide-solid max-w-6xl mx-auto">
     <h1 class="font-bold text-3xl pb-5 text-center underline underline-offset-4 tablet:text-4xl browser:text-6xl">Welcome to App Heading</h1>
     <div class="grid justify-items-center py-5">
         <img src="/main_banner.jpg" alt="banner"/>
