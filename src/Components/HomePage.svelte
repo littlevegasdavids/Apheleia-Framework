f58a5aff5edb06103060d9527b082b39ddf7ea40
thesamegatsby <rcd999@protonmail.com>
2022-08-19 14:17:04 +0200

    Changed .gitingore file to include prisma Changed app heading name on homepage

--- src/Components/HomePage.svelte
+++ src/Components/HomePage.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="grid divide-y divide-solid max-w-6xl mx-auto">
-    <h1 class="font-bold text-3xl pb-5 text-center underline underline-offset-4 tablet:text-4xl browser:text-6xl">Welcome to App Heading</h1>
+    <h1 class="font-bold text-3xl pb-5 text-center underline underline-offset-4 tablet:text-4xl browser:text-6xl">Welcome to Emporium Of Curiosity</h1>
     <div class="grid justify-items-center py-5">
         <img src="/main_banner.jpg" alt="banner" class="rounded-md shadow-lg"/>
     </div>
