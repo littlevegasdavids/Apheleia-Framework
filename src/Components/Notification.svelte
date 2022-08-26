762333a7f55acc736f8f524e9d41e0baa5f59416
thesamegatsby <rcd999@protonmail.com>
2022-08-26 12:47:29 +0200

    Centered notification message

--- src/Components/Notification.svelte
+++ src/Components/Notification.svelte
@@ -1,7 +1,7 @@
 <script>
     import {show_notification, message} from '../Stores/notification'
     import {fade} from 'svelte/transition'
-    import { onDestroy, onMount } from 'svelte';
+    import { onMount } from 'svelte';
 
     onMount(()=>{
         setTimeout(()=>{
@@ -11,5 +11,5 @@
 </script>
 
 <div in:fade={{duration: 100 }} out:fade={{duration: 500 }} class="grid absolute shadow-xl left-9 justify-items-center bg-success w-10/12 rounded-lg p-3 font-bold tablet:text-xl tablet:w-8/12 tablet:left-36 browser:w-6/12 browser:left-80">
-    <p>{$message}</p>
+    <p class="text-center">{$message}</p>
 </div>
