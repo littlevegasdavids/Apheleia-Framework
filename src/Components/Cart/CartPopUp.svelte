fa9c244d4588caa0d87923b185b1991d21076f2d
thesamegatsby <rcd999@protonmail.com>
2022-10-11 12:01:11 +0200

    Changed colour of cart popup

--- src/Components/Cart/CartPopUp.svelte
+++ src/Components/Cart/CartPopUp.svelte
@@ -15,9 +15,9 @@
 
 
 {#if $cart_items.length != 0}
-    <div tabindex="0" class="mt-10 card card-compact dropdown-content w-52 bg-base-100 shadow">
+    <div tabindex="0" class="mt-10 card card-compact dropdown-content w-52 bg-gray-200 shadow">
         <div class="card-body">
-            <div class="divide-y-2 divide-solid tablet:text-lg">
+            <div class="divide-y-2 divide-solid divide-black tablet:text-lg">
                 {#each $cart_items as item}
                     <div class="grid py-2">
                         <p>{item.Product.name}</p>
