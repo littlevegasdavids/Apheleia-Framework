97a89aa86ede7431e3b6c5c0a8a5f8e89334e60b
thesamegatsby <rcd999@protonmail.com>
2022-10-27 13:39:56 +0200

    Added socials links to footer

--- src/Components/Footer.svelte
+++ src/Components/Footer.svelte
@@ -1,4 +1,8 @@
 <footer class="footer footer-center gap-2 justify-items-center p-4 bg-gray-200 text-lg text-black">
+    <div class="grid grid-cols-2 gap-4">
+        <a href="https://www.instagram.com/emporiumofcuriosity/" target="_blank"><i class="fa-brands fa-instagram pr-3"></i>Instagram</a>
+        <a href="https://www.facebook.com/EmporiumofCuriosity/" target="_blank"><i class="fa-brands fa-facebook pr-3"></i>Facebook</a>
+    </div>
     <div>
         <span>
             <i class="fa-solid fa-envelope pr-3"></i>support@shopemporium.co.za
